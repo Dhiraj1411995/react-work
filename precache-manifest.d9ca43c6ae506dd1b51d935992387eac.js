bbaa4dbdb28b36ed268cee058dcb8c87872c491e
dhiraj-githubac <dhiraj.kumar@smscountry.com>
2020-09-12 17:09:08 +0530

    Updates

---
+++ precache-manifest.d9ca43c6ae506dd1b51d935992387eac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e9080173ceae9d3ef8e9a2584930ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "14451ca7b2956536699f",
+    "url": "/monsters-rolodex/static/css/main.9f56a133.chunk.css"
+  },
+  {
+    "revision": "8ee32475cd34a633133d",
+    "url": "/monsters-rolodex/static/js/2.20b2a995.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.20b2a995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14451ca7b2956536699f",
+    "url": "/monsters-rolodex/static/js/main.da89277a.chunk.js"
+  },
+  {
+    "revision": "5d9871b43329dda10fc8",
+    "url": "/monsters-rolodex/static/js/runtime-main.98ead556.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
